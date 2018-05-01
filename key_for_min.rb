6a63ad058008845820ccb0740ff749790bd2d3dc
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-01 08:05:24 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,8 @@
 def key_for_min_value(name_hash)
   min_value = name_hash.map {|key, value| value}.sort[0]
   min_key = nil
-  name_hash.each do |key, value| 
-    min_value = value if value < min_value
-    min_key = key
+  name_hash.each do |key, value|
+    min_key = key if value < min_value
   end 
   min_key
 end
